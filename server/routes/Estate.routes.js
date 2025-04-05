b40941111a55e1582f475d708579dd4975a046ac
Wiem0000 <hajriiwiem@gmail.com>
2025-04-05 19:06:07 +0100

    EstateDetails done

--- server/routes/Estate.routes.js
+++ server/routes/Estate.routes.js
@@ -1,8 +1,9 @@
 const express = require("express")
 const EstateRouter = express.Router()
-const {getAllEstate} = require("../controllers/Estate.controller")
+const {getAllEstate, getEstateById } = require("../controllers/Estate.controller")
 
 EstateRouter.get("/getall",getAllEstate)
+EstateRouter.get("/get/:id",getEstateById)
 
 
 module.exports = EstateRouter 
